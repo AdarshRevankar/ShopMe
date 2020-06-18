7c10285b29132627cf8c880479f3b1f6b007fdb2
AdarshRevankar <adarsh_revankar@live.com>
2020-06-18 17:00:17 +0530

    [RecyclerView]: Added a RecyclerView for the visualization of the results. 1. Recycler View to show the results searched 2. Optimized UI, using the Thread

--- app/src/main/java/com/adrino/shopme/ItemAdapter.kt
+++ app/src/main/java/com/adrino/shopme/ItemAdapter.kt
@@ -1,57 +1,32 @@
 package com.adrino.shopme
 
-import android.os.AsyncTask
 import android.os.StrictMode
 import android.util.Log
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
 
-
-private val mobiles = mutableListOf<Mobile>()
-
-fun getMobilesList() {
-    mobiles.add(Mobile(1, "Redmi K20", 21000))
-    mobiles.add(Mobile(1, "Redmi Note 9 Pro", 15000))
-    mobiles.add(Mobile(1, "OnePlus 7", 30000))
-    mobiles.add(Mobile(1, "Samsung S20", 79000))
-}
-
-fun searchPhone(keyword: String): MutableList<Mobile> {
-    if (mobiles.size == 0)
-        getMobilesList()
-    val resultList = mutableListOf<Mobile>()
-
-    for (mobile in mobiles) {
-        if (mobile.getName().startsWith(keyword)) {
-            resultList.add(mobile)
-        }
-        Log.e("SearchRes", "searchPhone" + mobile.getName())
-    }
-    Log.e("Search", "searchPhone: " + resultList.size)
-    return resultList
-}
-
 /**
  * Manages the SQL Queries
  */
 class SQLHandler(
-    private val user: String, private val password: String, private val database: String,
+    private val user: String,
+    private val password: String,
+    private val database: String,
     private val server: String
-) :
-    AsyncTask<String, String, String>() {
+) {
+
     private val connection = getConnectionObject(user, password, database, server)
-    public val mobilesList = mutableListOf<Mobile>()
 
     // Background Process
-    override fun doInBackground(vararg query: String?): String? {
+    fun executeQuery(query: String): MutableList<Mobile>? {
+        val mobilesList = mutableListOf<Mobile>()
         val statement = connection?.createStatement()
-        val res = statement?.executeQuery(query[0].toString())
+        val res = statement?.executeQuery(query)
         while (res != null && res.next())
             mobilesList.add(Mobile(1, res.getString("name"), 2000))
-        return null
+        return mobilesList
     }
-
 
     // Getting connection object
     private fun getConnectionObject(
@@ -60,15 +35,14 @@ class SQLHandler(
         database: String,
         server: String
     ): Connection? {
-        val policy =
-            StrictMode.ThreadPolicy.Builder().permitAll().build()
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
         var connection: Connection? = null
+        val url = "jdbc:mysql://$server:3306/$database"
 
         try {
             Class.forName("com.mysql.jdbc.Driver")
-            connection =
-                DriverManager.getConnection("jdbc:mysql://$server:3306/$database", user, password)
+            connection = DriverManager.getConnection(url, user, password)
         } catch (se: SQLException) {
             Log.e("SQL Execution Error : ", se.message)
         } catch (e: ClassNotFoundException) {
--- app/src/main/java/com/adrino/shopme/MainActivity.kt
+++ app/src/main/java/com/adrino/shopme/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.adrino.shopme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), RecyclerViewAdapter.ItemClickListener{
+    private var sqlHandler: SQLHandler? = null
+    private var recyclerView: RecyclerView? = null
+    private var adapter: RecyclerViewAdapter? = null
+    var animalNames = ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,14 +21,23 @@ class MainActivity : AppCompatActivity() {
         val searchView = findViewById<SearchView>(R.id.search)
         searchView.setOnQueryTextListener(queryListener)
 
-        val sqlHandler = SQLHandler(
+        sqlHandler = SQLHandler(
             resources.getString(R.string.db_user),
             resources.getString(R.string.db_password),
             resources.getString(R.string.db_database),
             resources.getString(R.string.db_server)
         )
 
-        sqlHandler.execute("SELECT name FROM characters")
+        animalNames.add("Akash")
+        animalNames.add("Akash")
+        animalNames.add("Akash")
+        animalNames.add("Akash")
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView?.setLayoutManager(LinearLayoutManager(this));
+        adapter = RecyclerViewAdapter(this, animalNames);
+        adapter?.setClickListener(this)
+        recyclerView?.setAdapter(adapter);
     }
 
     /**
@@ -30,23 +46,43 @@ class MainActivity : AppCompatActivity() {
     private val queryListener = object : SearchView.OnQueryTextListener {
         override fun onQueryTextSubmit(query: String?): Boolean {
             // When OK is pressed
+            if (query != null) {
+
+                Thread {
+                    val mobileList =
+                        sqlHandler?.executeQuery("SELECT name FROM characters where name like '%$query%'")
+                    updateRecyclerView(mobileList)
+                }.start()
+                return true
+            }
             return false
         }
 
         override fun onQueryTextChange(query: String?): Boolean {
             // On Each Key-press
             if (query != null) {
-                val resultList = searchPhone(query)
-                var resultString = ""
-                for (mobile in resultList)
-                    resultString += mobile.getName() + " "
-                Log.e("String", "onQueryTextChange: "+resultString )
-                runOnUiThread(
-                    Runnable { findViewById<TextView>(R.id.mobileTextView).setText(resultString) }
-                )
+                Thread {
+                    val mobileList =
+                        sqlHandler?.executeQuery("SELECT name FROM characters where name like '%$query%'")
+                    updateRecyclerView(mobileList)
+                }.start()
                 return true
             }
             return false
         }
+    }
+
+    fun updateRecyclerView(mobileList: MutableList<Mobile>?){
+        animalNames.clear()
+        if (mobileList == null)
+            return
+
+        for (m in mobileList)
+            animalNames.add(m.toString())
+        runOnUiThread {adapter?.notifyDataSetChanged()}
+    }
+
+    override fun onItemClick(view: View?, position: Int) {
+        TODO("Not yet implemented")
     }
 }
